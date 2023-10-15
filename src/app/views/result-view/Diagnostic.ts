41314dbc62de1c573f9cc8cce26da9b96c427f6b
Guadalupe <guadalupenicol102@gmail.com>
2023-10-14 23:03:05 -0300

    Pantalla de Resultado

---
+++ src/app/views/result-view/Diagnostic.ts
@@ -0,0 +1,14 @@
+// diagnostico.ts
+export class Diagnostic {
+    constructor(
+        public doctor: string,
+        public age: string,
+        public weight: string,
+        public height: string,
+        public gender: string,
+        public sectionBody: string,
+        public preconditions: string,
+        public resultDiagnostic: string,
+    ){}
+    
+}
--- src/app/views/result-view/result-view.component.ts
+++ src/app/views/result-view/result-view.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Diagnostic}from './Diagnostic';
 
 @Component({
   selector: 'app-result-view',
   templateUrl: './result-view.component.html',
   styleUrls: ['./result-view.component.css']
 })
-export class ResultViewComponent {
+export class ResultViewComponent implements OnInit {
+  diagnostic: Diagnostic = new Diagnostic("","","","","","","","");
   responseData: any;
 
   constructor() {
   }
+  
 
-  ngOnInit() {
+  ngOnInit(): void {
+      this.diagnostic.doctor = "";
+      this.diagnostic.age = "";
+      this.diagnostic.weight="";
+      this.diagnostic.height ="";
+      this.diagnostic.gender ="";
+      this.diagnostic.sectionBody = "";
+      this.diagnostic.preconditions = "";
+      this.diagnostic.resultDiagnostic = "";
+  
     const storedResponseData = localStorage.getItem('responseData');
     if (storedResponseData) {
       this.responseData = JSON.parse(storedResponseData);
