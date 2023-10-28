ff3d3f789d8f28bcff1191afee9093719d8ba1bd
Guadalupe <guadalupenicol102@gmail.com>
2023-10-28 16:11:28 -0300

    Responsive de contacto

--- src/app/models/Comment.ts
+++ src/app/models/Comment.ts
@@ -3,8 +3,8 @@ export class Comments {
   constructor(
 
     public name?: string,
-    public lastname?: string,
     public email?: string,
+    public issue?: string,
     public message?: string
 
   ) { }
