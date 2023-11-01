fdfa775e891a95ef0e22e29bffaa5b06c8f37aa0
Agustina Insua <aguss.insua99@gmail.com>
2023-11-01 19:46:05 -0300

    diseño del filtro

--- src/app/views/historial-view/historial-view.component.ts
+++ src/app/views/historial-view/historial-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, untracked } from '@angular/core';
 import { ResultService } from 'src/app/services/result/result.service';
 
 @Component({
@@ -11,6 +11,8 @@ export class HistorialViewComponent{
 
     tituloDinamico = 'Historial';
     calendarIcon = 'pi pi-calendar';
+    filteredData: any[] = [];
+    isFilter = false;
     cambiarTitulo(nuevoTitulo: string) {
     this.tituloDinamico = nuevoTitulo;
   }
@@ -27,31 +29,52 @@ export class HistorialViewComponent{
         this.dateTime.setDate(this.dateTime.getDate());
         this.data = [
             {
-                code: '1',
-                date: '2023-10-18',
+                code: 1,
+                date: '2023-10-08',
                 study: 'Cerebro',
                 doctor: 'Pérez',
             },
             {
-                code: '2',
-                date: '2023-09-18',
+                code: 2,
+                date: '2023-09-08',
                 study: 'Cerebro',
                 doctor: 'Arroyo',
             },
             {
-              code: '3',
-              date: '2023-10-18',
+              code: 3,
+              date: '2023-06-08',
               study: 'Corazon',
               doctor: 'Insua',
           },
           {
-              code: '4',
+              code: 4,
               date: '2023-03-15',
               study: 'Rodilla',
               doctor: 'Mendez',
           },
         ];
     }
+    applyDateFilter() {
+      if (this.rangeDates) {
+        this.isFilter = true;
+        const startDate = this.rangeDates[0];
+        const endDate = this.rangeDates[1];
+
+        this.filteredData = this.data.filter((item: any) => {
+          const itemDate = new Date(item.date);
+          return itemDate >= startDate && itemDate <= endDate;
+        });
+      } else {
+        this.filteredData = this.data;
+      }
+    }
+
+    clearFilter() {
+      this.isFilter = false;
+      this.rangeDates = undefined; // Limpia el rango de fechas (si estás usando rangeDates)
+      this.filteredData = this.data; // Restablece los datos filtrados
+    }
+
     private getRecordAll() {
         this.resultService.getRecordAll("98","4").subscribe({
             next: (res) => {
