d54b08ba0eb35ebdaf5228b8a7c0c75ca20b2792
Guadalupe <guadalupenicol102@gmail.com>
2023-11-02 18:55:02 -0300

    Botones dinámicos de feedback

--- src/app/views/result-view/result-view.component.ts
+++ src/app/views/result-view/result-view.component.ts
@@ -11,6 +11,9 @@ import { ResultcDTO } from 'src/app/models/Dtos/ResultDTO';
   styleUrls: ['./result-view.component.css']
 })
 export class ResultViewComponent implements OnInit {
+  buttonText1 !: string;
+  buttonText2 !: string;
+  botonesHabilitados: boolean = false;
   doctor !: any;
   diagnostic!: any;
   responseData: any;
@@ -18,28 +21,68 @@ export class ResultViewComponent implements OnInit {
   tituloDinamico = 'Resultado';
   formattedDate: any;
 
-    cambiarTitulo(nuevoTitulo: string) {
-    this.tituloDinamico = nuevoTitulo;
-  }
+  
 
   constructor(private resultDTO: ResultcDTO) {
   }
 
 
   ngOnInit(): void {
-this.diagnostic = new Diagnostic();
-this.doctor = new Doctor();
-  /*    this.diagnostic = this.resultDTO.getCompanyInformation();
-      const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
-      this.formattedDate = this.diagnostic.dateOfBirth.toLocaleDateString('es-ES', options); // Puedes cambiar 'es-ES' según tu preferencia de idioma
-      console.log(this.formattedDate);
-      console.log(this.diagnostic);
-
-    const storedResponseData = localStorage.getItem('responseData');
-    if (storedResponseData) {
-      this.responseData = JSON.parse(storedResponseData);
-      // Haz algo con los datos recibidos
-      console.log(this.responseData);
-    } */
+    this.diagnostic = new Diagnostic();
+    this.diagnostic.sectionBody = "Cerebro";
+    this.doctor = new Doctor();
+    this.buttonCase();
+    /*    this.diagnostic = this.resultDTO.getCompanyInformation();
+        const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
+        this.formattedDate = this.diagnostic.dateOfBirth.toLocaleDateString('es-ES', options); // Puedes cambiar 'es-ES' según tu preferencia de idioma
+        console.log(this.formattedDate);
+        console.log(this.diagnostic);
+  
+      const storedResponseData = localStorage.getItem('responseData');
+      if (storedResponseData) {
+        this.responseData = JSON.parse(storedResponseData);
+        // Haz algo con los datos recibidos
+        console.log(this.responseData);
+      } */
   }
+
+
+  buttonEnabled() {
+    this.botonesHabilitados = true;
+    
+  }
+  buttonCase() {
+    switch (this.diagnostic.sectionBody) {
+      case 'Cerebro':
+        this.buttonText1 = 'Hematoma';
+        this.buttonText2 = 'Otro';
+        break;
+      case 'Pulmones':
+        this.buttonText1 = 'Neumonía';
+        this.buttonText2 = 'Otro';
+        break;
+      case 'Corazon':
+        this.buttonText1 = 'Ritmia';
+        this.buttonText2 = 'Otro';
+        break;
+      case 'Riñon':
+        this.buttonText1 = 'Infeccion urinaria';
+        this.buttonText2 = 'Otro';
+        break;
+      case 'Rodilla':
+        this.buttonText1 = 'Artritis';
+        this.buttonText2 = 'Otro';
+        break;
+      case 'Muñeca':
+        this.buttonText1 = 'Quebradura';
+        this.buttonText2 = 'Otro';
+        break;
+      default:
+        this.buttonText1 = 'Botón 1';
+        this.buttonText2 = 'Botón 2';
+        break;
+    }
+    
+  }
+
 }
