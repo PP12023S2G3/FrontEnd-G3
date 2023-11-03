46a4535eca7405a177dbc629e6113677be08d51d
Soledad Ramirez <liasoledad27@gmail.com>
2023-11-03 19:19:35 -0300

    fix bug register spinner

--- src/app/views/registro-view/registro-view.component.ts
+++ src/app/views/registro-view/registro-view.component.ts
@@ -114,6 +114,7 @@ export class RegistroComponent {
       detail: 'Datos erroneos por favor corregir',
       life: 2000,
     });
+    this.loaderService.updateIsLoading(false);
   }
   }
   else {
@@ -123,6 +124,7 @@ export class RegistroComponent {
       detail: 'Completa todos los campos del formulario antes de continuar',
       life: 2000,
     });
+    this.loaderService.updateIsLoading(false);
   }
 }
 
