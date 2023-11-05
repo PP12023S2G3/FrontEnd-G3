d3ab5c2bd48c3291f940e3aab4255b84a1e41335
Guadalupe <guadalupenicol102@gmail.com>
2023-11-05 15:06:05 -0300

    Aguege los checkboxes a condiciones previas de resultado

--- src/app/views/result-view/result-view.component.ts
+++ src/app/views/result-view/result-view.component.ts
@@ -16,9 +16,9 @@ export class ResultViewComponent implements OnInit {
   buttonTextLabel1 !: string;
   buttonTextLabel2 !: string;
   buttonsCases: boolean = true;
-  buttonDownload : boolean = true;
-  buttonNo : boolean = false;
-  buttonYes : boolean = false;
+  buttonDownload: boolean = true;
+  buttonNo: boolean = false;
+  buttonYes: boolean = false;
 
   doctor !: any;
   diagnostic!: any;
@@ -27,15 +27,21 @@ export class ResultViewComponent implements OnInit {
   tituloDinamico = 'Resultado';
   formattedDate: any;
 
+  checkboxState: { [key: string]: boolean } = {};
 
+  checkboxes: { [key: string]: string[] } = {
+    Cerebro: ['Pérdida visual', 'Debilidad focal', 'Convulsiones'],
+    Pulmón: ['Puntada lateral', 'Fiebre', 'Dificultad respiratoria'],
+    Riñón: ['Sangre en orina', 'Dolor en zona lumbar', 'Cansancio'],
+  };
 
-  constructor(private resultDTO: ResultcDTO,private resultService: ResultService,private feedbackService: FeedbackService) {
+  constructor(private resultDTO: ResultcDTO, private resultService: ResultService, private feedbackService: FeedbackService) {
   }
 
 
   ngOnInit(): void {
-//ejemplo de como llamar al servicio de feedback
-    const reqFeedbackBrain=this.feedbackService.createRequestFeedbackBrain(1,true,true,true,true,"comentario");
+    //ejemplo de como llamar al servicio de feedback
+    const reqFeedbackBrain = this.feedbackService.createRequestFeedbackBrain(1, true, true, true, true, "comentario");
 
     this.feedbackService.postFeedbackBrain(reqFeedbackBrain).subscribe({
       next: (res) => {
@@ -49,11 +55,11 @@ export class ResultViewComponent implements OnInit {
     const idResult = localStorage.getItem('idResult');
     const roleId = localStorage.getItem('role');
 
-    if (idResult&&roleId) {
+    if (idResult && roleId) {
       // Realizar la llamada a tu servicio aquí, por ejemplo:
-        this.resultService.getRecord(parseInt(idResult),roleId).subscribe({
+      this.resultService.getRecord(parseInt(idResult), roleId).subscribe({
         next: (res) => {
-           // la respuesta de aca contiene todo lo necesario para mostrar en resultado
+          // la respuesta de aca contiene todo lo necesario para mostrar en resultado
           console.log(res);
         },
         error: (error) => {
@@ -61,8 +67,9 @@ export class ResultViewComponent implements OnInit {
         }
       });
     }
+
     this.diagnostic = new Diagnostic();
-    this.diagnostic.sectionBody = "Riñon";
+    this.diagnostic.sectionBody = "Riñón";
     this.doctor = new Doctor();
 
     this.changeLabelCases();
@@ -79,6 +86,22 @@ export class ResultViewComponent implements OnInit {
         console.log(this.responseData);
       } */
   }
+  
+
+// Función para registrar cambios en las opciones seleccionadas y obtener el estado de cada opción
+onCheckboxChange( option: string) {
+  const opcionesSeleccionadas = this.checkboxState;
+    const opcionesPermitidas = this.checkboxes[this.diagnostic.sectionBody];
+    const opcionesMostradas = opcionesPermitidas.map(
+      (opcion) =>
+        `${opcion}=${opcionesSeleccionadas[opcion] ? 'true' : 'false'}`
+    );
+
+    console.log(
+      'Datos enviados con éxito. Opciones enviadas: ' +
+        opcionesMostradas.join(', ')
+    );
+}
 
   disableButton(buttonDisable: number) {
     if (buttonDisable === 1) {
@@ -100,11 +123,11 @@ export class ResultViewComponent implements OnInit {
         this.buttonTextLabel1 = 'Neumonía';
         this.buttonTextLabel2 = 'Otro';
         break;
-      case 'Corazon':
+      case 'Corazón':
         this.buttonTextLabel1 = 'Ritmia';
         this.buttonTextLabel2 = 'Otro';
         break;
-      case 'Riñon':
+      case 'Riñón':
         this.buttonTextLabel1 = 'Infeccion urinaria';
         this.buttonTextLabel2 = 'Otro';
         break;
@@ -124,8 +147,9 @@ export class ResultViewComponent implements OnInit {
 
   }
 
-  enableButtonDownload(){
+  enableButtonDownload() {
     this.buttonDownload = false; //habilitar
   }
+
 
 }
