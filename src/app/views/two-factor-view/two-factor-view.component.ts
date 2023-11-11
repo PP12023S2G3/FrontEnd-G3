7f26dc82790d35ec5312c373a71b39710930f30f
Soledad Ramirez <liasoledad27@gmail.com>
2023-11-10 23:15:33 -0300

    ruteo recuperar contraseña

--- src/app/views/two-factor-view/two-factor-view.component.ts
+++ src/app/views/two-factor-view/two-factor-view.component.ts
@@ -8,14 +8,24 @@ import { MessageService } from 'primeng/api';
   providers: [MessageService],
 })
 export class TwofactorViewComponent {
+  value: string = '';
+  correctLength: boolean = false;
+  modalVisible: boolean = false;
 
   constructor(private messageService: MessageService) {
   }
-  value: string = '';
-  correctLength: boolean = false;
 
   isCorrectInput(): void {
     this.correctLength = this.value.length === 6;
   }
+
+  showModal() {
+    this.modalVisible = true;
+    this.desenfocarFondo();
+  }
+  desenfocarFondo() {
+    var containerBlur = document.querySelector(".body-two-factor");
+      containerBlur != null ? containerBlur.classList.add("blur-div"): ""; 
+  }  
 
 }
