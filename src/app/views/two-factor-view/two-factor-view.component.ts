390ee3680d914a5dab443a24e16c4c0766efb3f4
Agustina Insua <aguss.insua99@gmail.com>
2023-11-10 21:06:06 -0300

    estructura de validaciones

--- src/app/views/two-factor-view/two-factor-view.component.ts
+++ src/app/views/two-factor-view/two-factor-view.component.ts
@@ -1,22 +1,27 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-two-factor-view',
   templateUrl: './two-factor-view.component.html',
-  styleUrls: ['./two-factor-view.component.css']
+  styleUrls: ['./two-factor-view.component.css'],
+  providers: [MessageService],
 })
 export class TwofactorViewComponent {
   modalVisible: boolean = false;
 
+  constructor(private messageService: MessageService) {
+  }
+
   mostrarModal() {
     this.modalVisible = true;
     this.desenfocarFondo();
-    
+
 }
 
 desenfocarFondo() {
    var containerBlur = document.querySelector(".body-two-factor");
-   containerBlur != null ? containerBlur.classList.add("blur-div"): ""; 
-} 
+   containerBlur != null ? containerBlur.classList.add("blur-div"): "";
+}
 
 }
