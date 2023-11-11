ba56e9a41cccdc71394c06d6062d43168af8a4f0
Guadalupe <guadalupenicol102@gmail.com>
2023-11-11 15:46:44 -0300

    Validaciones de contacto

--- src/app/views/contacto-view/contacto-view.component.ts
+++ src/app/views/contacto-view/contacto-view.component.ts
@@ -3,6 +3,7 @@ import { MessageService } from 'primeng/api';
 import { Comments } from 'src/app/models/Comment';
 
 
+
 @Component({
   selector: 'app-contacto-view',
   templateUrl: './contacto-view.component.html',
@@ -14,16 +15,18 @@ export class ContactoViewComponent implements OnInit {
   cambiarTitulo(nuevoTitulo: string) {
     this.tituloDinamico = nuevoTitulo;
   }
+
   comments!: Comments;
-  messageOptions : {label : string; value: string;}[] | undefined;
+  messageOptions: { label: string; value: string; }[] | undefined;
 
   constructor(private messageService: MessageService) {
+
   }
 
   ngOnInit(): void {
     this.messageOptions = [
-      {label: 'tipo 1', value : 'valor tipo 1'},
-      {label: 'tipo 2', value: 'valor tipo 2'}
+      { label: 'tipo 1', value: 'valor tipo 1' },
+      { label: 'tipo 2', value: 'valor tipo 2' }
     ];
 
 
@@ -31,12 +34,51 @@ export class ContactoViewComponent implements OnInit {
 
   }
 
-  OnSelectedMessage(event : any) {
+  OnSelectedMessage(event: any) {
   }
+
+
+
 
   onSubmit() {
+    this.validateName();
+    this.validateEmail();
+    this.validateMessage();
 
   }
+
+  validateName() {
+    if (!this.comments.name) {
+      return alert('Por favor complete el nombre.');
+    }
+    if (this.comments.name.length >= 100 || this.comments.name.match(/\d/)) {
+
+      return alert('Nombre y apellido deben tener menos de 100 caracteres y no deben contener números.');
+    }
+  }
+  validateEmail() {
+    if (!this.comments.email) {
+      return alert('Por favor complete el email.');
+    }
+    if (!this.comments.email.includes('@') || !this.comments.email.includes('.com')) {
+
+      return alert('El email debe contener el signo "@" y terminar con ".com"');
+    }
+    if (this.comments.email.length >= 100) {
+      return alert('El email debe tener menos de 100 caracteres.');
+
+    }
+  }
+  validateMessage() {
+    if (!this.comments.message) {
+      return alert('Por favor complete el mensaje.');
+    }
+    if (this.comments.message.length > 300) {
+
+      return alert('El mensaje debe tener menos de 300 caracteres.');
+    }
+  }
+
 
 
 }
