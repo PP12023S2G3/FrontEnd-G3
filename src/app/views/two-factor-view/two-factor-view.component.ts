46bd81e63bfe811ce655d7becdbf5702c614ff04
Soledad Ramirez <liasoledad27@gmail.com>
2023-11-13 21:55:29 -0300

    fix validacion código nueva contraseña

--- src/app/views/two-factor-view/two-factor-view.component.ts
+++ src/app/views/two-factor-view/two-factor-view.component.ts
@@ -44,7 +44,6 @@ export class TwofactorViewComponent {
     const numericRegex = /^[0-9]+$/;
     if (!numericRegex.test(this.code)) {
       this.messageService.add({ severity: 'error', summary: 'Error', detail: 'El código debe contener sólo números' });
-      this.showModal();
     } else {
       this.postCheckCode();
     }
